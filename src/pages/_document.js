125192a6a6cd812638df56e01552e3ec2b53319b
zwood011 <zwood011@gmail.com>
2024-12-31 18:51:16 -0500

    Added bootstrap CDN

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+        />
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+          defer
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
