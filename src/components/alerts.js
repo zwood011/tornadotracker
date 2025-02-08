6386f1348f0fad5aaf832a3a5e4353a6dc0540f6
zwood011 <zwood011@gmail.com>
2025-02-08 03:58:02 -0700

    Added latest label for most recent tornado alert

--- src/components/alerts.js
+++ src/components/alerts.js
@@ -7,12 +7,13 @@ const Alerts = ({ result }) => {
         // Update tornado alerts when the tornado result is available
         if (result) {
             if (result.alerts?.length > 0) {
-                setTornadoAlerts(result.alerts.map(alert => ({
+                setTornadoAlerts(result.alerts.map((alert, index) => ({
                     certainty: alert?.properties?.certainty || null,
                     headline: alert?.properties?.headline || null,
                     description: alert?.properties?.description || null,
                     severity: alert?.properties?.severity || null,
                     status: alert?.properties?.status || null,
+                    latest: index === 0 ? 'LATEST' : null
                 })));
             } else {
                 setTornadoAlerts(null); // No alerts available
@@ -32,6 +33,7 @@ const Alerts = ({ result }) => {
                         <ul className="alerts-list">
                             {tornadoAlerts.map((alert, index) => (
                                 <li className="p-3 m-3 border d-flex flex-column w-50" key={index}>
+                                    <p><em>{alert.latest}</em></p>
                                     <h3>{alert.headline}</h3>
                                     <p>{alert.description}</p>
                                     <p>Severity: {alert.severity}</p>
